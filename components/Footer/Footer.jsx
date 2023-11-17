0db566c25807cc0c6aebb40b045ee2e84caf9f0a
uidev-in <mail2loveseries@gmail.com>
2023-11-17 13:52:45 +0530

    added hero section

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,28 +2,29 @@
 
 import { Poppins } from "next/font/google";
 import {
-  overview,features,support,marvel,follow
+  overview,
+  features,
+  support,
+  marvel,
+  follow,
 } from "@/constant/footer.constant";
 import { Marvel } from "../Icons/Marvel";
-
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 function Footer() {
-
-
   return (
-    <footer className="py-5 lg:py-20 bg-black border-t border-primary-green">
+    <footer className="py-5 lg:py-20 bg-primary-gray border-t border-primary-green">
       {/* desktop footer */}
       <section className="max-w-7xl mx-auto px-5 hidden md:flex">
         <div className="w-[150px] lg:w-[250px] flex justify-center">
           <a href="/" aria-label="Marvel">
-          <Marvel className={"w-24"} fill="#fff"/>
+            <Marvel className={"w-24"} fill="#fff" />
           </a>
         </div>
         <div className=" pl-5 lg:w-full md:flex lg:flex-wrap justify-evenly lg:-mt-3">
           <ul>
-            <span  className="block pb-3">Overview</span>
+            <span className="block pb-3">Overview</span>
             {overview.map((row) => (
               <li key={row.name}>
                 <a
@@ -36,7 +37,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Features</span>
+            <span className="block pb-3">Features</span>
             {features.map((row) => (
               <li key={row.name}>
                 <a
@@ -49,7 +50,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Support</span>
+            <span className="block pb-3">Support</span>
             {support.map((row) => (
               <li key={row.name}>
                 <a
@@ -62,7 +63,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Marvel</span>
+            <span className="block pb-3">Marvel</span>
             {marvel.map((row) => (
               <li key={row.name}>
                 <a
@@ -75,7 +76,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Follow</span>
+            <span className="block pb-3">Follow</span>
             {follow.map((row) => (
               <li key={row.name}>
                 <a
@@ -88,21 +89,19 @@ function Footer() {
             ))}
           </ul>
         </div>
-      
       </section>
-
 
       {/* mobile footer */}
       <section className="px-10 md:hidden">
         <div className="mb-10">
           <a href="/" aria-label="Marvel">
-          <Marvel className={"w-24"} fill="#fff"/>
+            <Marvel className={"w-24"} fill="#fff" />
           </a>
         </div>
-        
+
         <div className="flex flex-wrap gap-10">
           <ul>
-            <span  className="block pb-3">Overview</span>
+            <span className="block pb-3">Overview</span>
             {overview.map((row) => (
               <li key={row.name}>
                 <a
@@ -115,7 +114,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Features</span>
+            <span className="block pb-3">Features</span>
             {features.map((row) => (
               <li key={row.name}>
                 <a
@@ -128,7 +127,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Support</span>
+            <span className="block pb-3">Support</span>
             {support.map((row) => (
               <li key={row.name}>
                 <a
@@ -141,7 +140,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Marvel</span>
+            <span className="block pb-3">Marvel</span>
             {marvel.map((row) => (
               <li key={row.name}>
                 <a
@@ -154,7 +153,7 @@ function Footer() {
             ))}
           </ul>
           <ul className="">
-          <span className="block pb-3">Follow</span>
+            <span className="block pb-3">Follow</span>
             {follow.map((row) => (
               <li key={row.name}>
                 <a
@@ -167,7 +166,6 @@ function Footer() {
             ))}
           </ul>
         </div>
-      
       </section>
     </footer>
   );
---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
+
+export default function Hero() {
+  return (
+    <section className="py-20 bg-primary-gray">
+      <div className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-7">
+          <div className="px-5 lg:10">
+            <div className="flex justify-between lg:min-h-[448px]">
+              <div className="py-6 md:py-12">
+                <h1 className="px-2 text-center lg:max-w-3xl lg:text-start font-semibold text-3xl lg:text-6xl">
+                  Let's build the future of design, now.
+                </h1>
+                <p className="text-md mt-4 text-center lg:block lg:text-start lg:pl-3  lg:max-w-lg">
+                  Create amazing tools and integrations for 1 million users or
+                  customize Marvel for your team.
+                </p>
+                <div className="my-8 max-w-xl flex gap-2">
+                  <button
+                    className={`${poppins.className} bg-primary-blue font-medium text-base px-5 lg:px-7 py-3 rounded-full`}
+                  >
+                    Submit your app
+                  </button>
+                  <button
+                    className={`${poppins.className} bg-white font-medium text-black text-base px-5 lg:px-7 py-3 rounded-full`}
+                  >
+                    View API Docs
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-7">
+            <img
+              className="object-cover"
+              src="/hero.png"
+              width={560}
+              height={460}
+              alt="hero"
+              loading="lazy"
+              decoding="async"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
